875d79e5b72b0e6a9c7adf8e28c55da4e59370c2
JeffreyLiu <17771770679@163.com>
2023-02-11 18:25:09 +0800

    chore: 4-2添加TS类型定义

---
+++ src/screens/project-list/index.tsx
@@ -0,0 +1,41 @@
+import { List } from "./list";
+import { SearchPanel } from "./search-panel";
+import { useEffect, useState } from "react";
+import qs from "qs";
+import { cleanObject, useDebounce } from "../../utils";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+export const ProjectListScreen = () => {
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const [users, setUsers] = useState([]);
+  const [list, setList] = useState([]);
+  const debouncedParam = useDebounce(param, 200);
+
+  useEffect(() => {
+    fetch(
+      `http://localhost:3001/projects?${qs.stringify(
+        cleanObject(debouncedParam)
+      )}`
+    ).then(async (res) => {
+      if (res.ok) {
+        setList(await res.json());
+      }
+    });
+  }, [debouncedParam]);
+  useEffect(() => {
+    fetch(`http://localhost:3001/users`).then(async (res) => {
+      if (res.ok) {
+        setUsers(await res.json());
+      }
+    });
+  }, []);
+  return (
+    <div>
+      <SearchPanel param={param} setParam={setParam} users={users} />
+      <List list={list} users={users} />
+    </div>
+  );
+};
---
+++ src/screens/project-list/list.tsx
@@ -0,0 +1,37 @@
+import { User } from "./search-panel";
+export interface Project {
+  id: string;
+  name: string;
+  personId: string;
+  email: string;
+  organization: string;
+}
+interface ListProps {
+  list: Project[];
+  users: User[];
+}
+
+export const List = ({ list, users }: ListProps) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>名称</th>
+          <th>负责人</th>
+        </tr>
+      </thead>
+      <tbody>
+        {list.map((project) => {
+          return (
+            <tr key={project.id}>
+              <td>{project.name}</td>
+              <td>
+                {users.find((user) => user.id === project.personId)?.name}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
