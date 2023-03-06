884496ed993978e85d8271f8c4048b56531b04d6
Jeffrey <17771770679@163.com>
2023-03-06 22:25:14 +0800

    feat: 6-1使用 antd组件库

--- src/unauthenticated-app/register.tsx
+++ src/unauthenticated-app/register.tsx
@@ -1,27 +1,38 @@
+import { Button, Form, Input } from "antd";
+import Card from "antd/lib/card";
 import { useAuth } from "context/auth-context";
-import React, { FormEvent } from "react";
+import React from "react";
 
 export const RegisterScreen = () => {
-  const { register, user } = useAuth()
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const username = (event.currentTarget.elements[0] as HTMLInputElement)
-      .value;
-    const password = (event.currentTarget.elements[1] as HTMLInputElement)
-      .value;
-    register({ username, password });
+  const { register } = useAuth()
+  const handleSubmit = (values: { username: string, password: string }) => {
+    register(values);
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="username">用户名</label>
-        <input type={"text"} id={"username"} />
-      </div>
-      <div>
-        <label htmlFor="username">密码</label>
-        <input type={"password"} id={"password"} />
-      </div>
-      <button type="submit">注册</button>
-    </form>
+    <Form onFinish={handleSubmit}>
+      <Card>
+        <Form.Item
+          name={"username"}
+          rules={[{ required: true, message: "请输入用户名" }]}
+        >
+          <Input placeholder={"用户名"} type="text" id={"username"} />
+        </Form.Item>
+        <Form.Item
+          name={"password"}
+          rules={[{ required: true, message: "请输入密码" }]}
+        >
+          <Input placeholder={"密码"} type="password" id={"password"} />
+        </Form.Item>
+        <Form.Item>
+          {/* <LongButton loading={isLoading} htmlType={"submit"} type={"primary"}>
+          登录
+        </LongButton> */}
+          <Button htmlType={"submit"} type={"primary"}>
+            注册
+          </Button>
+        </Form.Item>
+      </Card>
+
+    </Form>
   );
 };
