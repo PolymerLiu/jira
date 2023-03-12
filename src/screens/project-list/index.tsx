b04463f4e1d30a3289f4a8b3eaea83ba4bbe8b62
Jeffrey <17771770679@163.com>
2023-03-12 09:12:58 +0800

    feat: 7-2使用useAsync统一处理loading和error状态

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,49 +1,27 @@
 import React from "react";
 import { List } from "./list";
 import { SearchPanel } from "./search-panel";
-import { useEffect, useState } from "react";
-import qs from "qs";
-import { cleanObject, useDebounce } from "../../utils";
-import { useHttp } from "utils/http";
+import { useState } from "react";
+import { useDebounce } from "../../utils";
 import styled from "@emotion/styled";
 import { Typography } from "antd";
+import { useProjects } from "utils/project";
+import { useUsers } from "utils/user";
 
 export const ProjectListScreen = () => {
   const [param, setParam] = useState({
     name: "",
     personId: "",
   });
-  const [users, setUsers] = useState([]);
-  const [list, setList] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<null | Error>(null);
   const debouncedParam = useDebounce(param, 200);
-  const client = useHttp()
-
-  useEffect(() => {
-    setIsLoading(true)
-    client('projects', {
-      data: cleanObject(debouncedParam)
-    }).then(setList)
-      .catch((err) => {
-        setList([])
-        setError(err)
-      })
-      .finally(() => {
-        setIsLoading(false)
-      })
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [debouncedParam]);
-  useEffect(() => {
-    client('users').then(setUsers)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const { isLoading, error, data: list } = useProjects(debouncedParam)
+  const { data: users } = useUsers(debouncedParam)
   return (
     <Container>
       <h1>项目列表</h1>
-      <SearchPanel param={param} setParam={setParam} users={users} />
+      <SearchPanel param={param} setParam={setParam} users={users || []} />
       {error ? <Typography.Text type="danger">{error.message}</Typography.Text> : ''}
-      <List dataSource={list} users={users} loading={isLoading} />
+      <List dataSource={list || []} users={users || []} loading={isLoading} />
     </Container>
   );
 };
